f8a7e3e7924450c4d1e1455328b4cef3aa68abfc
MyeongSeok-oh <mung000221@gmail.com>
2025-11-19 12:03:13 +0900

    251119

---
+++ 사칙연산.py
@@ -0,0 +1,35 @@
+'''
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+'''
+A, B = map(int, input().split())
+print(A + B)
+
+'''
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+'''
+A, B = map(int, input().split())
+print(A - B)
+
+'''
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+'''
+A, B = map(int, input().split())
+print(A * B)
+
+'''
+두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+'''
+A, B = map(int, input().split())
+print(A / B)
+
+'''
+두 정수 A와 B를 입력받은 다음, A/B(몫)를 출력하는 프로그램을 작성하시오.
+'''
+A, B = map(int, input().split())
+print(A // B)
+
+'''
+두 정수 A와 B를 입력받은 다음, A%B(나머지)를 출력하는 프로그램을 작성하시오.
+'''
+A, B = map(int, input().split())
+print(A % B)
